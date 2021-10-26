072666d09e85d65e3f7699fffae6613e209734c5
Travis Benedict <benedtra@amazon.com>
2021-10-26 12:09:24 -0500

    Remove environment specific java_resolver tests
    Signed-off-by: Travis Benedict <benedtra@amazon.com>

--- tests/builder/java_resolver_test.py
+++ tests/builder/java_resolver_test.py
@@ -51,33 +51,6 @@ class JavaResolverTests(TestCase):
         self.assertEqual(java_home, "/opt/jdk8")
         resolve_jvm_path.assert_called_with([8])
 
-    # NOTICE: This test is dependent on the operating system you have. Uncomment out and
-    # delete the next test when OpenSearch has a distribution for MacOS
-    # def test_resolves_java_home_for_bundled_jdk(self):
-    #     major, java_home = java_resolver.java_home("12,11,10,9,8",
-    #                                                specified_runtime_jdk="bundled",
-    #                                                provides_bundled_jdk=True)
-
-    #     # assumes most recent JDK
-    #     self.assertEqual(major, 12)
-    #     # does not set JAVA_HOME for the bundled JDK
-    #     self.assertEqual(java_home, None)
-
-    # Darwin is the operating system for MacOS and since OpenSearch does not
-    # currently support MacOS, have to use a hacky method to make sure it gets
-    # a proper JDK (this is because OpenSearch comes with a JDK that is not supported on MacOS as well)
-    # def test_resolves_java_home_for_bundled_jdk_darwin(self):
-    #     major, java_home = java_resolver.java_home("12,11,10,9,8",
-    #                                                specified_runtime_jdk="bundled",
-    #                                                provides_bundled_jdk=True)
-
-    #     # Make sure you have already set JAVA_HOME to JDK 11 path in your venv or it will throw a key error
-    #     java_home_set = os.getenv("JAVA_HOME")
-    #     # assumes most recent JDK
-    #     self.assertEqual(major, 11)
-    #     # sets JAVA_HOME to JAVA_HOME env
-    #     self.assertEqual(java_home, java_home_set)
-
     @mock.patch("osbenchmark.utils.sysstats.os_name", return_value="Windows")
     def test_resolves_java_home_for_bundled_jdk_windows(self, os_name):
         with self.assertRaises(exceptions.SystemSetupError) as ctx:
