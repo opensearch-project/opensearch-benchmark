5247e32b49a85d58f2199a9a0ec475664d8d8d1c
Chase Engelbrecht <engechas@amazon.com>
2022-03-09 13:47:34 -0600

    Update Launcher interface to include the metrics store in the stop method. Add NoOpLauncher implementation
    Signed-off-by: Chase Engelbrecht <engechas@amazon.com>

--- osbenchmark/builder/launchers/launcher.py
+++ osbenchmark/builder/launchers/launcher.py
@@ -1,9 +1,13 @@
-"""
-Launchers are used to start and stop OpenSearch on the nodes in a self-managed cluster.
-"""
+import logging
+
+
 class Launcher:
+    """
+    Launchers are used to start and stop OpenSearch on the nodes in a self-managed cluster.
+    """
     def __init__(self, executor):
         self.executor = executor
+        self.logger = logging.getLogger(__name__)
 
     def start(self, host, node_configurations):
         """
@@ -15,12 +19,13 @@ class Launcher:
         """
         raise NotImplementedError
 
-    def stop(self, host, nodes):
+    def stop(self, host, nodes, metrics_store):
         """
         Stops the OpenSearch nodes on a given host
 
         ;param host: A Host object defining the host on which to stop the nodes
         ;param nodes: A list of Node objects defining the nodes running on a host
+        ;param metrics_store: A reference to the metrics store, used for collecting telemetry data before stopping the nodes
         ;return nodes: A list of Node objects representing OpenSearch nodes that were successfully stopped on the host
         """
         raise NotImplementedError
