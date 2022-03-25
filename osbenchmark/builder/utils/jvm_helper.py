655268e171bcc19d18da8790d621b7562d5d648d
Chase Engelbrecht <engechas@amazon.com>
2022-03-24 19:21:24 -0500

    Refactor JvmHelper to use executor, remove unused methods, simplify existing methods
    Signed-off-by: Chase Engelbrecht <engechas@amazon.com>

---
+++ osbenchmark/builder/utils/jvm_helper.py
@@ -0,0 +1,99 @@
+import os
+import re
+
+from osbenchmark.exceptions import SystemSetupError
+from osbenchmark.utils import io
+
+
+class JvmHelper:
+    def __init__(self, executor):
+        self.executor = executor
+
+    def resolve_path(self, host, majors):
+        """
+        Resolves the path to the JDK with the provided major version(s). It checks the versions in the same order specified in ``majors``
+        and will return the first match. To achieve this, it first checks the major version x in the environment variable ``JAVAx_HOME``
+        and falls back to ``JAVA_HOME``. It also ensures that the environment variable points to the right JDK version.
+
+        If no appropriate version is found, a ``SystemSetupError`` is raised.
+
+        :param host: The host on which to resolve the JVM path
+        :param majors: Either a list of major versions to check or a single version as an ``int``.
+        :return: A tuple of (major version, path to Java home directory).
+        """
+        if isinstance(majors, int):
+            return majors, self._resolve_path(host, [majors])
+        else:
+            return majors, self._resolve_path(host, majors)
+
+    def _resolve_path(self, host, majors):
+        """
+        Resolves the path to a JDK with one of the provided major versions.
+
+        :param majors: The major versions to check.
+        :return: The resolved path to the JDK
+        """
+
+        defined_env_vars = self._get_defined_env_vars(host)
+        java_home_env_var_names = ["JAVA{}_HOME".format(major) for major in majors]
+        java_home_env_var_names.append("JAVA_HOME")
+
+        resolved_major_to_java_home_path = {}
+        for java_home_env_var_name in java_home_env_var_names:
+            if java_home_env_var_name in defined_env_vars:
+                major_to_java_home_path = self._resolve_major_from_java_home(host, defined_env_vars[java_home_env_var_name])
+                if major_to_java_home_path:
+                    resolved_major_to_java_home_path.update(major_to_java_home_path)
+
+        for major in majors:
+            if major in resolved_major_to_java_home_path:
+                return resolved_major_to_java_home_path[major]
+
+        raise SystemSetupError("Install a JDK with one of the versions {} and point to it with one of {}."
+                                          .format(majors, self._checked_env_vars(majors)))
+
+    def _get_defined_env_vars(self, host):
+        env_vars_as_strings = self.executor.execute(host, "printenv", output=True)
+        return dict(env_var_as_string.split("=") for env_var_as_string in env_vars_as_strings)
+
+    def _resolve_major_from_java_home(self, host, java_home):
+        return {self._major_version(host, java_home): java_home} if java_home else None
+
+    def _major_version(self, host, java_home):
+        """
+        Determines the major version number of JVM available at the provided JAVA_HOME directory.
+
+        :param java_home: The JAVA_HOME directory to check.
+        :return: An int, representing the major version number of the JVM available at ``java_home``.
+        """
+        version = self._system_property(host, java_home, "java.vm.specification.version")
+        # are we under the "old" (pre Java 9) or the new (Java 9+) version scheme?
+        if version.startswith("1."):
+            return int(version[2])
+        else:
+            return int(version)
+
+    def _system_property(self, host, java_home, system_property_name):
+        lines = self.executor.execute(host, "{} -XshowSettings:properties -version".format(self._java(java_home)), output=True)
+        # matches e.g. "    java.runtime.version = 1.8.0_121-b13" and captures "1.8.0_121-b13"
+        sys_prop_pattern = re.compile(r".*%s.*=\s?(.*)" % system_property_name)
+        for line in lines:
+            m = sys_prop_pattern.match(line)
+            if m:
+                return m.group(1)
+
+        return None
+
+    def _java(self, java_home):
+        return io.escape_path(os.path.join(java_home, "bin", "java"))
+
+    def _checked_env_vars(self, majors):
+        """
+        Provides a list of environment variables that are checked for the given list of major versions.
+
+        :param majors: A list of major versions.
+        :return: A list of checked environment variables.
+        """
+        checked = ["JAVA{}_HOME".format(major) for major in majors]
+        checked.append("JAVA_HOME")
+        return checked
---
+++ tests/builder/utils/jvm_helper_test.py
@@ -0,0 +1,57 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from osbenchmark.builder.utils.jvm_helper import JvmHelper
+from osbenchmark.exceptions import SystemSetupError
+
+
+class JvmHelperTests(TestCase):
+    def setUp(self):
+        self.host = None
+        self.executor = Mock()
+        self.jvm_helper = JvmHelper(self.executor)
+
+    def test_success_pre_java_9(self):
+        # printenv, $JAVA_HOME -XshowSettings:properties -version
+        self.executor.execute.side_effect = [["JAVA7_HOME=/fake/path"], ["java.vm.specification.version = 1.7.0"]]
+
+        _, jvm_path = self.jvm_helper.resolve_path(self.host, 7)
+        self.assertEqual("/fake/path", jvm_path)
+
+    def test_success_post_java_8(self):
+        # printenv, $JAVA_HOME -XshowSettings:properties -version
+        self.executor.execute.side_effect = [["JAVA9_HOME=/fake/path"], ["java.vm.specification.version = 9"]]
+
+        _, jvm_path = self.jvm_helper.resolve_path(self.host, 9)
+        self.assertEqual("/fake/path", jvm_path)
+
+    def test_generic_java_home_matches(self):
+        # printenv, $JAVA_HOME -XshowSettings:properties -version
+        self.executor.execute.side_effect = [["JAVA_HOME=/fake/path"], ["java.vm.specification.version = 9"]]
+
+        _, jvm_path = self.jvm_helper.resolve_path(self.host, 9)
+        self.assertEqual("/fake/path", jvm_path)
+
+    def test_multiple_majors(self):
+        # printenv, $JAVA_HOME -XshowSettings:properties -version x 2
+        self.executor.execute.side_effect = [
+            ["JAVA_HOME=/fake/path", "JAVA14_HOME=/another/fake/path"], ["java.vm.specification.version = 14"],
+            ["java.vm.specification.version = 9"]
+        ]
+
+        _, jvm_path = self.jvm_helper.resolve_path(self.host, [8, 14, 16])
+        self.assertEqual("/another/fake/path", jvm_path)
+
+    def test_no_matching_version(self):
+        # printenv, $JAVA_HOME -XshowSettings:properties -version
+        self.executor.execute.side_effect = [["JAVA_HOME=/fake/path"], ["java.vm.specification.version = 9"]]
+
+        with self.assertRaises(SystemSetupError):
+            self.jvm_helper.resolve_path(self.host, 10)
+
+    def test_no_java_home_set(self):
+        # printenv, $JAVA_HOME -XshowSettings:properties -version
+        self.executor.execute.side_effect = [[]]
+
+        with self.assertRaises(SystemSetupError):
+            self.jvm_helper.resolve_path(self.host, 10)
