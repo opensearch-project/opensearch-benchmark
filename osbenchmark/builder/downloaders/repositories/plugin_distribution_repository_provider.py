cfd891398d0a5120484d983a2bc1eb4360ce0b8c
Chase Engelbrecht <engechas@amazon.com>
2022-04-11 15:54:25 -0500

    Implement ExternalPluginSourceDownloader, refactor repo url lookup in PluginDistributionRepositoryProvider
    Signed-off-by: Chase Engelbrecht <engechas@amazon.com>

--- osbenchmark/builder/downloaders/repositories/plugin_distribution_repository_provider.py
+++ osbenchmark/builder/downloaders/repositories/plugin_distribution_repository_provider.py
@@ -9,5 +9,5 @@ class PluginDistributionRepositoryProvider:
     def get_download_url(self, host):
         distribution_repository = self.plugin.variables["distribution"]["repository"]
 
-        default_key = f"plugin.{self.plugin.name}.{distribution_repository}.url"
+        default_key = f"distribution.{distribution_repository}.remote.repo.url"
         return self.repository_url_provider.render_url_for_key(host, self.plugin.variables, default_key, mandatory=False)
