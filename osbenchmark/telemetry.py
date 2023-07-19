5f88caa74e770f3e519222f5f743bb4f34690ae3
Ayush Verma <ayuverm@amazon.com>
2023-07-19 11:02:25 +0530

    Added comment to specify ES fix
    Signed-off-by: Ayush Verma <ayuverm@amazon.com>

--- osbenchmark/telemetry.py
+++ osbenchmark/telemetry.py
@@ -626,6 +626,7 @@ class NodeStats(TelemetryDevice):
 
     def on_benchmark_start(self):
         default_client = self.clients["default"]
+        # ElasticSearch does not supply a value for the distribution field
         distribution_name = default_client.info()["version"].get("distribution", "")
         distribution_version = default_client.info()["version"]["number"]
         major, minor = components(distribution_version)[:2]
