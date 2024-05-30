0228b6e6c50e4ed00aa15fece52cc183bcb156a8
Ian Hoang <hoangia@amazon.com>
2024-05-29 21:14:20 -0500

    Create-Workload V2: Updates Further Refactoring
    Signed-off-by: Ian Hoang <hoangia@amazon.com>

--- osbenchmark/workload_generator/config.py
+++ osbenchmark/workload_generator/config.py
@@ -4,8 +4,7 @@ from typing import List
 @dataclass
 class Index:
     name: str
-    settings: dict
-    mappings: dict
+    settings_and_mappings: dict
     document_frequency: int
     limit_documents: int
 
@@ -20,9 +19,8 @@ class CustomWorkload:
     workload_name: str
     root_path: str
     indices: List[Index]
-    failed_corpus: List[Index]
+    failed_indices: List[Index]
     queries: List[str]
     workload_path: str
     operations_path: str
     test_procedures_path: str
-
