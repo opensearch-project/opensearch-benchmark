e8745b4195cdef4568958d1e054a54e4412745f3
Martin Gaievski <gaievski@amazon.com>
2024-07-29 10:51:54 -0700

    Added runners for enabling concurrent segment search via settings (#591)
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

--- osbenchmark/workload/workload.py
+++ osbenchmark/workload/workload.py
@@ -635,6 +635,7 @@ class OperationType(Enum):
     DeleteMlModel = 1041
     RegisterMlModel = 1042
     DeployMlModel = 1043
+    UpdateConcurrentSegmentSearchSettings = 1044
 
     @property
     def admin_op(self):
@@ -752,6 +753,8 @@ class OperationType(Enum):
             return OperationType.TrainKnnModel
         elif v == "delete-knn-model":
             return OperationType.DeleteKnnModel
+        elif v == "update-concurrent-segment-search-settings":
+            return OperationType.UpdateConcurrentSegmentSearchSettings
         else:
             raise KeyError(f"No enum value for [{v}]")
 
