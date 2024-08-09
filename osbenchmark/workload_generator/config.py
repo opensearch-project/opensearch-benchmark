e79599f63b186b9ba2de12948c766409742df36a
Ian Hoang <51065478+IanHoang@users.noreply.github.com>
2024-08-08 21:16:50 -0500

    [Create-Workload Enhancements] Rearchitect Create-Workload Feature (#609)
    Signed-off-by: Ian Hoang <hoangia@amazon.com>
    Co-authored-by: Ian Hoang <hoangia@amazon.com>

---
+++ osbenchmark/workload_generator/config.py
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+# Modifications Copyright OpenSearch Contributors. See
+# GitHub history for details.
+
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class Index:
+    name: str = None
+    document_frequency: int = 1
+    number_of_docs: int = None
+    settings_and_mappings: dict = field(default_factory=dict)
+
+@dataclass
+class CustomWorkload:
+    workload_name: str = None
+    output_path: str = None
+    workload_path: str = None
+    operations_path: str = None
+    test_procedures_path: str = None
+    indices: List[Index] = field(default_factory=list)
+    extracted_indices: List[str] = field(default_factory=list)
+    failed_indices: List[str] = field(default_factory=list)
+    corpora: List[dict] = field(default_factory=list)
+    queries: List[dict] = field(default_factory=list)
--- tests/workload_generator/corpus_test.py
+++ tests/workload_generator/corpus_test.py
@@ -22,78 +22,88 @@
 # specific language governing permissions and limitations
 # under the License.
 import json
-from unittest import mock
-from unittest.mock import call
+from unittest import mock, TestCase
+from unittest.mock import call, Mock
 
-from osbenchmark.workload_generator import corpus
+from osbenchmark.workload_generator.config import CustomWorkload
+from osbenchmark.workload_generator.extractors import SequentialCorpusExtractor
 
+class TestSequentialCorpusExtractor(TestCase):
 
-def serialize_doc(doc):
-    return (json.dumps(doc, separators=(",", ":")) + "\n").encode("utf-8")
+    def setUp(self):
+        self.mock_custom_workload = Mock(spec=CustomWorkload)
+        self.mock_custom_workload.workload_path = "/abs/outpath/to/workloads/"
+        # pylint: disable=no-value-for-parameter
+        self.mock_client = self.create_mock_client()
+        self.corpus_extractor = SequentialCorpusExtractor(self.mock_custom_workload, self.mock_client)
 
+    @mock.patch("opensearchpy.OpenSearch")
+    def create_mock_client(self, client):
+        return client
 
-@mock.patch("builtins.open", new_callable=mock.mock_open)
-@mock.patch("opensearchpy.OpenSearch")
-def test_extract(client, mo):
-    doc = {
-        "field1": "stuff",
-        "field2": "things"
-    }
-    doc_data = serialize_doc(doc)
-    client.count.return_value = {
-        "count": 1001
-    }
-    client.search.return_value = {
-        "_scroll_id": "uohialjrknf",
-        "_shards": {
-            "successful": 1,
-            "total": 1,
-            "skipped": 0
-        },
-        "hits": {
-            "hits": [
-                {
-                    "_index": "test",
-                    "_id": "0",
-                    "_score": 0,
-                    "_source": doc
-                }
-            ]
+    def serialize_doc(self, doc):
+        return (json.dumps(doc, separators=(",", ":")) + "\n").encode("utf-8")
+
+    @mock.patch("builtins.open", new_callable=mock.mock_open)
+    def test_extract(self, mo):
+        doc = {
+            "field1": "stuff",
+            "field2": "things"
         }
-    }
+        doc_data = self.serialize_doc(doc)
+        self.mock_client.count.return_value = {
+            "count": 1001
+        }
+        self.mock_client.search.return_value = {
+            "_scroll_id": "uohialjrknf",
+            "_shards": {
+                "successful": 1,
+                "total": 1,
+                "skipped": 0
+            },
+            "hits": {
+                "hits": [
+                    {
+                        "_index": "test",
+                        "_id": "0",
+                        "_score": 0,
+                        "_source": doc
+                    }
+                ]
+            }
+        }
 
-    def set_corp_size(*args, **kwargs):
-        path = args[0]
-        mockstat = mock.Mock()
-        if ".bz2" in path:
-            mockstat.st_size = 500
-        else:
-            mockstat.st_size = 1000
-        return mockstat
+        def set_corp_size(*args, **kwargs):
+            path = args[0]
+            mockstat = mock.Mock()
+            if ".bz2" in path:
+                mockstat.st_size = 500
+            else:
+                mockstat.st_size = 1000
+            return mockstat
 
-    client.scroll.return_value = {}
+        self.mock_client.scroll.return_value = {}
 
-    index = "test"
-    outdir = "/abs/outpath/to/workloads/"
+        index = "test"
 
-    with mock.patch("os.stat") as osstat:
-        osstat.side_effect = set_corp_size
-        res = corpus.extract(client, outdir, index)
-    assert mo.call_count == 4
-    mo.assert_has_calls([call("/abs/outpath/to/workloads/test-documents.json", "wb"),
-                         call("/abs/outpath/to/workloads/test-documents.json.bz2", "wb"),
-                         call("/abs/outpath/to/workloads/test-documents-1k.json", "wb"),
-                         call("/abs/outpath/to/workloads/test-documents-1k.json.bz2", "wb")
-                         ], any_order=True)
+        with mock.patch("os.stat") as osstat:
+            osstat.side_effect = set_corp_size
+            res = self.corpus_extractor.extract_documents(index)
+        assert mo.call_count == 4
+        mo.assert_has_calls([call("/abs/outpath/to/workloads/test-documents.json", "wb"),
+                            call("/abs/outpath/to/workloads/test-documents.json.bz2", "wb"),
+                            call("/abs/outpath/to/workloads/test-documents-1k.json", "wb"),
+                            call("/abs/outpath/to/workloads/test-documents-1k.json.bz2", "wb")
+                            ], any_order=True)
 
-    assert res == {
-        "filename": "test-documents.json.bz2",
-        "path": "/abs/outpath/to/workloads/test-documents.json.bz2",
-        "compressed_bytes": 500,
-        "index_name": "test",
-        "doc_count": 1001,
-        "uncompressed_bytes": 1000
-    }
+        assert res == {
+            "filename": "test-documents.json.bz2",
+            "path": "/abs/outpath/to/workloads/test-documents.json.bz2",
+            "compressed_bytes": 500,
+            "index_name": "test",
+            "doc_count": 1001,
+            "uncompressed_bytes": 1000
+        }
 
-    file_mock = mo.return_value
-    file_mock.assert_has_calls([call.write(doc_data)])
+        file_mock = mo.return_value
+        file_mock.assert_has_calls([call.write(doc_data)])
