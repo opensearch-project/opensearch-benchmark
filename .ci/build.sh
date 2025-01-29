af60c87dc2c1f8bd27c00172da53672fa1a6609e
Michael Oviedo <mikeovi@amazon.com>
2025-01-29 14:58:50 -0800

    update to JDK 21 for integ tests (#737)
    Signed-off-by: Michael Oviedo <mikeovi@amazon.com>

--- .ci/build.sh
+++ .ci/build.sh
@@ -12,6 +12,21 @@ function setup {
 
   # Init pyenv.
   PATH=$HOME/.pyenv/shims:$PATH:$HOME/.pyenv/bin
+
+  # OpenSearch has different JDK requirements:
+  # - Gradle builds need JDK 21 (after Apache Lucene 10 upgrade)
+  # - OpenSearch runtime operations need JDK 17
+  # Store the current JAVA_HOME (Java 21) for Gradle
+  export GRADLE_JAVA_HOME=$JAVA_HOME
+  
+  # Set JAVA_HOME to Java 17 for OpenSearch
+  if [ -n "$JAVA17_HOME" ]; then
+    echo "Setting JAVA_HOME to Java 17 for OpenSearch"
+    export JAVA_HOME=$JAVA17_HOME
+    java -version
+  else
+    echo "WARNING: JAVA17_HOME is not set!"
+  fi
 }
 
 function build_and_unit_test {
@@ -28,8 +43,23 @@ function run_it {
 
   docker pull ubuntu/squid:latest
 
-  # make it38, it39, etc. so they run as concurrent GHA jobs
+  # Temporarily switch to Java 21 for Gradle builds if needed
+  if [ -n "$GRADLE_JAVA_HOME" ]; then
+    OLD_JAVA_HOME=$JAVA_HOME
+    export JAVA_HOME=$GRADLE_JAVA_HOME
+    echo "Switched to Java 21 for Gradle build"
+    java -version
+  fi
+
+  # Run the integration test
   make "it${1//./}"
+
+  # Switch back to Java 17 if we changed it
+  if [ -n "$OLD_JAVA_HOME" ]; then
+    export JAVA_HOME=$OLD_JAVA_HOME
+    echo "Switched back to Java 17"
+    java -version
+  fi
 }
 
 $@
