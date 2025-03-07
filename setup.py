28238723c98767792592e48d5cc1a01b94e6ecd6
xuxiong1 <xiongxug@outlook.com>
2025-03-07 01:57:30 +0000

    update require aiokafka version
    Signed-off-by: xuxiong1 <xiongxug@outlook.com>

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ install_requires = [
     "numpy>=1.24.2,<=1.26.4",
     # License: Apache 2.0
     # Required for Kafka message producer
-    "aiokafka>=0.8.0",
+    "aiokafka>=0.12.0",
 ]
 
 tests_require = [
