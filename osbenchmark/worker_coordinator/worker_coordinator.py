929496490435779fbbca160a0f6a8410e070b81b
Michael Oviedo <mikeovi@amazon.com>
2025-07-03 10:43:22 -0700

    add mandatory=False flag for CPU feedback fetch (#884)
    Signed-off-by: Michael Oviedo <mikeovi@amazon.com>

--- osbenchmark/worker_coordinator/worker_coordinator.py
+++ osbenchmark/worker_coordinator/worker_coordinator.py
@@ -1000,7 +1000,7 @@ class WorkerCoordinator:
             self.target.on_cluster_details_retrieved(self.retrieve_cluster_info(os_clients))
 
         # Redline testing: Check if cpu feedback is enabled. Enable the node-stats telemetry device if we need to
-        cpu_max = self.config.opts("workload", "redline.max_cpu_usage", default_value=[])
+        cpu_max = self.config.opts("workload", "redline.max_cpu_usage", default_value=None, mandatory=False)
         if cpu_max:
             devices = self.config.opts("telemetry", "devices", default_value=[])
             if "node-stats" not in devices:
@@ -1100,7 +1100,7 @@ class WorkerCoordinator:
             metrics_index = None
             test_execution_id = None
             # we must have a metrics store connected for CPU based feedback
-            cpu_max = self.config.opts("workload", "redline.max_cpu_usage", default_value=None)
+            cpu_max = self.config.opts("workload", "redline.max_cpu_usage", default_value=None, mandatory=False)
             if cpu_max and isinstance(self.metrics_store, metrics.InMemoryMetricsStore):
                 raise exceptions.SystemSetupError("CPU-based feedback requires a metrics store. You are using an in-memory metrics store")
             elif cpu_max and "node-stats" not in self.config.opts("telemetry", "devices"):
