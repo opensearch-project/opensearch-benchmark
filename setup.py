2c882f898415e10992942d98988921dd982202ac
Ian Hoang <ianhoang16@gmail.com>
2025-08-13 13:45:46 -0500

    Add comma
    Signed-off-by: Ian Hoang <ianhoang16@gmail.com>

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ install_requires = [
     # Licence: BSD-3-Clause
     "bokeh!=3.0.*,>=2.4.2",
     # License: MIT
-    "pydantic>=2.10.6"
+    "pydantic>=2.10.6",
     # License: MIT
     "pydantic_core>=2.27.2"
 ]
