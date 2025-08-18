c0749f36dca8620e598c5e50fd89352ccb8f9781
Govind Kamat <govkamat@amazon.com>
2025-08-18 11:10:27 -0700

    Initial set of Data Reader changes for Streaming Ingestion (#935)
    Signed-off-by: Govind Kamat <govkamat@amazon.com>

--- osbenchmark/cloud_provider/vendors/s3_data_producer.py
+++ osbenchmark/cloud_provider/vendors/s3_data_producer.py
@@ -15,8 +15,6 @@ from boto3 import client
 
 from osbenchmark import exceptions
 from osbenchmark.data_streaming.data_producer import DataProducer
-from osbenchmark.cloud_provider import CloudProviderFactory
-from osbenchmark.cloud_provider.vendors.aws import AWSProvider
 from osbenchmark.workload.ingestion_manager import IngestionManager
 
 class S3DataProducer(DataProducer):
@@ -39,42 +37,21 @@ class S3DataProducer(DataProducer):
             # Defaults.  These may be overridden by the Ingestion Manager later.
             self.data_dir = data_dir or "/tmp"
             self.chunk_prefix = "chunk-"
-            self.chunk_size = 50 * 1024**2              # download 50 MB chunks
-            self.num_workers = 16                       # should be based on number of cores
+            self.chunk_size = IngestionManager.chunk_size * 1024**2
+            self.num_workers = os.cpu_count() * 2
 
-            # Credentials are set via the client options in a similar manner as used for
-            # the existing SigV4 support.
-            if client_options:
-                # TBD: use transient credentials that auto-refresh.
-                # We assume "amazon_aws_log_in" is set as one of the client options.
-                # provider = CloudProviderFactory.get_provider_from_client_options(client_options)
-                client_options["amazon_aws_log_in"] = "environment"
-                provider = CloudProviderFactory.providers[0]
-                if not isinstance(provider, AWSProvider):
-                    raise exceptions.ConfigError("S3DataProducer operates only on AWS")
-
-                # Extract credentials from the environment, as set in the OSB_* variables.
-                provider.parse_log_in_params(client_options=client_options)
-                masked_client_options = dict(client_options)
-                provider.mask_client_options(masked_client_options, client_options)
-                self.logger.info("Masking client options with cloud provider: [%s]", provider)
-
-                self.s3_client = client('s3',
-                                   aws_access_key_id = provider.aws_log_in_config['aws_access_key_id'],
-                                   aws_secret_access_key = provider.aws_log_in_config['aws_secret_access_key'],
-                                   aws_session_token = provider.aws_log_in_config['aws_session_token'],
-                                   )
-            else:
-                # For testing.  Set credentials with environment variables.
-                self.s3_client = client('s3')
+            self.s3_client = client('s3')
         except Exception as e:
             print(f"Error: {e}")
 
+    def _get_keys(self):
+        rsl = list()
+        rsl.append(self.keys)
+        return rsl
+
     def _get_next_downloader(self):
         "Generator that returns the downloader for the next object to be downloaded."
-        # Name globbing to be added later.
-        keys = [self.keys] if not isinstance(self.keys, list) else self.keys
-        for k in keys:
+        for k in self._get_keys():
             # Obtain the object size.
             response = self.s3_client.head_object(Bucket=self.bucket, Key=k)
             size = response['ContentLength']
