1288c8359f51c8fe26694b765d7c5c21daf57844
Finn <carrofin@amazon.com>
2026-03-20 11:13:16 -0700

    Update OSB with 3.5 gRPC schema changes (#1021)
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ install_requires = [
     "pydantic_core>=2.27.2",
     # License: Apache 2.0
     # gRPC & proto deps
-    "opensearch-protobufs==0.19.0"
+    "opensearch-protobufs==1.2.0"
 ]
 
 tests_require = [
